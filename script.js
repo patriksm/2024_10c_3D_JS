6cc552893c970d546fc57cbf4d11e5c1bd97e7f9
Patriks <pmorevs@gmail.com>
2024-04-11 09:04:25 +0300

    some changes to geometry

--- script.js
+++ script.js
@@ -10,23 +10,39 @@ var labirints = [
     [300, 40, 0, 78, 54, 45, 300, 400, "orange", 0.8],
 ];
 
-for(let i = 0; i < labirints.length; i++){
-    var newElement = document.createElement("div");
-    newElement.id = "element" + i;
-    newElement.style.position = "absolute";
-    newElement.style.background = labirints[i][8];
-    newElement.style.opacity = labirints[i][9];
-    newElement.style.width = `${labirints[i][6]}px`;
-    newElement.style.height = `${labirints[i][7]}px`;
-    newElement.style.transform = `
-        rotateX(${labirints[i][3]}deg) 
-        rotateY(${labirints[i][4]}deg) 
-        rotateZ(${labirints[i][5]}deg)
-        translate3d(
-                ${labirints[i][0]}px,
-                ${labirints[i][1]}px,
-                ${labirints[i][2]}px
-        )
-    `;
-    world.append(newElement);
+var lab1 = [
+    [0, 100, 0, 90, 0, 0, 2000, 2000, "red", 0.5]
+];
+
+function zimetLabirintu(map){
+    for(let i = 0; i < map.length; i++){
+        var newElement = document.createElement("div");
+        newElement.id = "element" + i;
+        newElement.style.position = "absolute";
+        newElement.style.background = map[i][8];
+        newElement.style.opacity = map[i][9];
+        newElement.style.width = `${map[i][6]}px`;
+        newElement.style.height = `${map[i][7]}px`;
+        newElement.style.transform = `
+            
+            translate3d(
+                    ${600 - map[i][6]/2 + map[i][0]}px,
+                    ${400 - map[i][7]/2 + map[i][1]}px,
+                    ${map[i][2]}px
+            )
+
+            rotateX(${map[i][3]}deg) 
+            rotateY(${map[i][4]}deg) 
+            rotateZ(${map[i][5]}deg)
+        `;
+        world.append(newElement);
+    }
 }
+
+zimetLabirintu(lab1);
+
+function update(){
+
+}
+
+let spele = setInterval(update, 10);
